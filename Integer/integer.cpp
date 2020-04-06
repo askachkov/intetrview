fc483bad564e50ac81af6b04f4338298f45e4c1f
Viktor Lepekha <v.v.l@ukr.net>
2020-04-06 21:26:29 +0300

    code fix

--- Integer/integer.cpp
+++ Integer/integer.cpp
@@ -1,7 +1,25 @@
-
 #include "integer.h"
+#include <iostream>
+
+static void log(const char * message)
+{
+    std::cout << "Obejct: [" << message << "]" << std::endl;
+}
 
 Integer::Integer(int32_t value)
 {
+    log("Integer Constructor");
     m_Value = value;
+}
+
+std::ostream& operator<< (std::ostream &out, const Integer &point)
+{
+    out << point.m_Value;
+    return out;
+}
+
+std::istream& operator>> (std::istream &in, Integer &point)
+{
+    in >> point.m_Value;
+    return in;
 }
--- Integer/integer.h
+++ Integer/integer.h
@@ -3,13 +3,25 @@
 
 #include "object.h"
 #include <cstdint> // C++ analog of stdint.h
+#include <iostream>
 
 class Integer:
         public Object
 {
 public:
     Integer(int32_t value = 0); // Default Ctor + Ctor with value
-
+    const Integer operator *(const Integer& a) const {
+        return (a.m_Value * m_Value);
+    }
+    const Integer operator %(const Integer& a) const {
+        return (a.m_Value % m_Value);
+    }
+    bool operator <(int32_t Value) const {
+        return (Value > m_Value);
+    }
+    int32_t getValue() {return m_Value;}
+    friend std::ostream& operator<< (std::ostream &out, const Integer &point);
+    friend std::istream& operator>> (std::istream &in, Integer &point);
 private:
     int32_t m_Value;
 };
