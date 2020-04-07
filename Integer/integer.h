cc15d4f9f708b8b43de298b9933b8b6142e59498
Bezkrovnyi <VBezkrovnyi@kiev.luxoft.com>
2020-04-07 13:07:27 +0300

    test implementation

--- Integer/integer.h
+++ Integer/integer.h
@@ -10,8 +10,41 @@ class Integer:
 public:
     Integer(int32_t value = 0); // Default Ctor + Ctor with value
 
+    Integer operator*(const int & a);
+    Integer operator%(const int & a);
+    Integer operator+(const int & a);
+    Integer operator-(const int & a);
+
+
+    Integer operator-(const  Integer& val);
+    Integer operator+(const  Integer& val);
+    Integer operator/( const  Integer& val);
+    bool operator==( const  Integer& val);
+
+    bool operator<(const int & a);
+    bool operator==(const int & a);
+
+    bool operator<(const  Integer& a );
+    bool operator<=(const  Integer& a);
+    bool operator>=(const  Integer& a);
+    bool operator>(const  Integer& a);
+
+    int32_t get_Value() const;
+    void set_Value(int32_t& value);
+
+    std::string toString();
+
+    void Print();
+
+    ~Integer();
+
+
 private:
     int32_t m_Value;
 };
+
+
+std::ostream& operator<<(std::ostream &out, const Integer &a);
+std::istream& operator>>(std::istream &in, Integer &a);
 
 #endif // INTEGER_H
