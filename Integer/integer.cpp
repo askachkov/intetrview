1390df13083c3bd091669eb9f962119e16a798ff
Yermak <SYermak@kiev.luxoft.com>
2020-04-07 17:15:19 +0300

    syermak variant

--- Integer/integer.cpp
+++ Integer/integer.cpp
@@ -1,7 +1,47 @@
 
 #include "integer.h"
+#include <iostream>
+
+using namespace std;
 
 Integer::Integer(int32_t value)
 {
     m_Value = value;
+}
+int32_t Integer::getValue() const {
+	return m_Value;
+}
+
+void Integer::setValue(int32_t value){
+	m_Value=value;
+}
+
+ostream& operator<<(ostream &out, const Integer value)
+{
+    out << value.getValue();
+    return out;
+}
+
+istream& operator>>(istream &input,Integer &value)
+{
+	int32_t inval;
+	input >> inval;
+	value.setValue(inval);
+    return input;
+}
+
+Integer &Integer::operator* (const Integer &value)
+{
+	setValue(getValue()*value.getValue());
+    return *this;
+}
+
+Integer &Integer::operator% (const Integer &value)
+{
+	setValue(getValue()%value.getValue());
+    return *this;
+}
+
+bool Integer::operator< (const Integer &value){
+	return (getValue() < value.getValue());
 }
--- Integer/integer.h
+++ Integer/integer.h
@@ -9,9 +9,17 @@ class Integer:
 {
 public:
     Integer(int32_t value = 0); // Default Ctor + Ctor with value
-
+    Integer &operator* (const Integer &value);
+    Integer &operator% (const Integer &value);
+    bool operator< (const Integer &value);
+    int32_t getValue() const;
+    void setValue(int32_t value);
 private:
     int32_t m_Value;
 };
+
+
+std::ostream& operator<<(std::ostream &out, const Integer value);
+std::istream& operator>>(std::istream &input,Integer &value);
 
 #endif // INTEGER_H
